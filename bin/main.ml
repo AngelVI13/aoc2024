035c55a27c1183aedde8225e79711f15276f0c72
AngelVI <a.v.iliev13@gmail.com>
2024-12-03 12:21:07 +0200

    done: day2 part2

--- bin/main.ml
+++ bin/main.ml
@@ -4,4 +4,5 @@ open! Aoc2024.Day2
 let () =
   (*Day1Part1.solve ()*)
   (*Day1Part2.solve ()*)
-  Day2Part1.solve ()
+  (*Day2Part1.solve ()*)
+  Day2Part2.solve ()
--- lib/day2.ml
+++ lib/day2.ml
@@ -31,3 +31,45 @@ module Day2Part1 = struct
     in
     printf "\n\n%d\n" safe
 end
+
+module Day2Part2 = struct
+  let is_safe entries =
+    let sorted_entries = List.sort ~compare:Int.compare entries in
+    let sorted_rev = List.rev sorted_entries in
+    match entries = sorted_entries || entries = sorted_rev with
+    | false -> false
+    | true ->
+        let level_differences =
+          List.map2_exn (List.drop_last_exn entries) (List.tl_exn entries)
+            ~f:(fun a b -> abs (a - b))
+        in
+        let invalid_differences =
+          List.filter level_differences ~f:(fun a -> a < 1 || a > 3)
+        in
+        List.length invalid_differences = 0
+
+  let rec dampener entries idx =
+    let new_entries =
+      let hd, tl = List.split_n entries idx in
+      let tl = List.tl_exn tl in
+      hd @ tl
+    in
+    match is_safe new_entries with
+    | true -> true
+    | false ->
+        if List.length entries - 1 = idx then false
+        else dampener entries (idx + 1)
+
+  let solve () =
+    let lines = In_channel.read_lines "./inputs/day2part2.txt" in
+    let safe =
+      List.fold ~init:0
+        ~f:(fun acc line ->
+          let entries = String.split line ~on:' ' in
+          let entries = List.map entries ~f:int_of_string in
+          let count = if is_safe entries || dampener entries 0 then 1 else 0 in
+          acc + count)
+        lines
+    in
+    printf "\n\n%d\n" safe
+end
